65690b7297314e56b8a6b61f3ac95903862fde5a
newspa1 <m123155527@gmail.com>
2025-04-07 02:51:30 +0800

    Finish homework3 problem1

--- hw3_b11902167/hw3.py
+++ hw3_b11902167/hw3.py
@@ -7,8 +7,15 @@ class Image:
         grid = grid.astype(int)
         bond = 0
         bond += (grid[0, 0] + grid[0, 2] + grid[2, 0] + grid[2, 2]) // 255
-        bond += 2* (grid[0, 1] + grid[1, 0] + grid[1, 2] + grid[2, 1]) // 255
+        bond += 2 * (grid[0, 1] + grid[1, 0] + grid[1, 2] + grid[2, 1]) // 255
         return bond
+
+    def getBoolGridBond(grid):
+        grid = grid.astype(int)
+        bond = 0
+        bond += (grid[0, 0] + grid[0, 2] + grid[2, 0] + grid[2, 2])
+        bond += 2 * (grid[0, 1] + grid[1, 0] + grid[1, 2] + grid[2, 1])
+        return bond        
 
     def getAllVariations(pattern):
         rotations = [np.rot90(pattern, k=i) for i in range(4)]
@@ -40,17 +47,42 @@ class Image:
             conditional_patterns[11] = np.array(
                 Image.getAllVariations([[255, 255, 255], [255, 255, 255], [255, 255, 0]])
             )
-
         return conditional_patterns
 
-    def denoise_Guassian(img, kernelsize):
-        kernel = np.ones((kernelsize, kernelsize), np.float32) / (kernelsize**2)
-        img_new = img.copy()
-        for r in range(kernelsize//2, img_new.shape[0] - kernelsize//2):
-            for c in range(kernelsize//2, img_new.shape[1] - kernelsize//2):
-                sub_img = img_new[r-kernelsize//2:r+kernelsize//2+1, c-kernelsize//2:c+kernelsize//2+1]
-                img_new[r, c] = np.sum(sub_img * kernel)
+    def maxmin(pixels):
+        maxpixel = pixels[0]
+        for index in range(len(pixels) - 2):
+            minpixel = min(pixels[index], pixels[index + 1], pixels[index + 2])
+            if minpixel > maxpixel:
+                maxpixel = minpixel
+        return maxpixel
+    
+    def minmax(pixels):
+        minpixel = pixels[0]
+        for index in range(len(pixels) - 2):
+            maxpixel = max(pixels[index], pixels[index + 1], pixels[index + 2])
+            if maxpixel < minpixel:
+                minpixel = maxpixel
+        return minpixel
+    
+    def getNeighbors(img, r, c, windowsize):
+        size = (img.shape[0], img.shape[1])
+        pixels = [img[r][c]]
+        for i in range(1, windowsize // 2 + 1):
+            pixels.append(img[max(0, r - i)][c])
+            pixels.append(img[min(size[0] - 1, r + i)][c])
+            pixels.append(img[r][max(0, c - i)])
+            pixels.append(img[r][min(size[1] - 1, c + i)])
+        return pixels
 
+    def denoise_Impulse(img, windowsize):
+        # PMED
+        img_new = np.zeros_like(img)
+        for r in range(img.shape[0]):
+            for c in range(img.shape[1]):
+                pixels = Image.getNeighbors(img, r, c, windowsize)
+                img_new[r, c] = Image.minmax(pixels)
+        
         return img_new
 
     def connect_gap(img):
@@ -136,75 +168,53 @@ class Image:
         return img_mark
 
     def isPatternHit(grid, type):
+        bond = Image.getBoolGridBond(grid)
         if type == "Skeletonize":
-            patterns = np.array(
-                Image.getAllVariations([[False, False, False], [False, True, False], [False, False, True]]) +
-                Image.getAllVariations([[False, False, False], [False, True, False], [False, True, False]]) + 
-                Image.getAllVariations([[False, False, False], [False, True, True], [False, True, False]])
-            )
-            for pattern in patterns:
-                if np.array_equal(grid, pattern):
+            if bond == 1:
+                return True
+            if bond == 2:
+                if grid[2, 1] or grid[1, 2] or grid[1, 0] or grid[0, 1]:
+                    return True
+            if bond == 4:
+                if ((grid[0, 1] and grid[1, 2]) or
+                    (grid[0, 1] and grid[1, 0]) or
+                    (grid[1, 2] and grid[2, 1]) or 
+                    (grid[1, 0] and grid[2, 1])):
+                    return True
+            if bond >= 5:
+                if ((grid[0, 0] and grid[0, 1] and grid[1, 0]) or
+                    (grid[1, 2] and grid[2, 1] and grid[2, 2])):
+                    return True
+            if bond >= 6:
+                if ((grid[0, 1] and grid[1, 0] and grid[1, 2]) or 
+                    (grid[0, 1] and grid[1, 0] and grid[2, 1]) or 
+                    (grid[1, 0] and grid[1, 2] and grid[2, 1]) or 
+                    (grid[0, 1] and grid[1, 2] and grid[2, 1])):
                     return True
 
-            patterns = np.array(
-                np.array([[True, True, False], [True, True, False], [False, False, False]]) +
-                np.array([[False, False, False], [False, True, True], [False, True, True]]) +
-                Image.getAllVariations([[False, True, False], [True, True, True], [False, False, False]]),
-                dtype=bool
-            )
-            for pattern in patterns:
-                isfind = True
-                for r in range(3):
-                    for c in range(3):
-                        if pattern[r, c] and not grid[r, c]:
-                            isfind = False
-                
-                if isfind:
-                    return True
-                
-            # print("second")
-            patterns = np.array(
-                Image.getAllVariations([[1, 2, 1], [2, 1, 2], [3, 3, 3]])
-            )
-            for pattern in patterns:
-                isfind = True
-                true1 = False
-                for r in range(3):
-                    for c in range(3):
-                        if pattern[r, c] == 1 and not grid[r, c]:
-                            isfind = False
-                        if pattern[r, c] == 3 and grid[r, c]:
-                            true1 = True
-
-                if isfind and true1:
+            if 5 <= bond <= 7:
+                if ((grid[0, 1] and grid[1, 2] and grid[2, 0] and not grid[0, 2] and not grid[1, 0] and not grid[2, 1]) or 
+                    (grid[0, 1] and grid[1, 0] and grid[2, 2] and not grid[0, 0] and not grid[1, 2] and not grid[2, 1]) or 
+                    (grid[0, 2] and grid[1, 0] and grid[2, 1] and not grid[0, 1] and not grid[1, 2] and not grid[2, 0]) or 
+                    (grid[0, 0] and grid[1, 2] and grid[2, 1] and not grid[0, 1] and not grid[1, 0] and not grid[2, 2])):
                     return True
             
-            patterns = np.array(
-                Image.getAllVariations([[2, 1, 0], [0, 1, 1], [1, 0, 2]])
-            )
-
-            for pattern in patterns:
-                isfind = True
-                for r in range(3):
-                    for c in range(3):
-                        if pattern[r, c] == 1 and not grid[r, c]:
-                            isfind = False
-                        if pattern[r, c] == 0 and grid[r, c]:
-                            isfind = False
-
-                if isfind:
+            if bond >= 3:
+                if ((grid[0, 0] and grid[0, 2] and (grid[2, 0] or grid[2, 1] or grid[2, 2])) or
+                    (grid[0, 0] and grid[2, 0] and (grid[0, 2] or grid[1, 2] or grid[2, 2])) or
+                    (grid[2, 0] and grid[2, 2] and (grid[0, 0] or grid[0, 1] or grid[0, 2])) or
+                    (grid[0, 2] and grid[2, 2] and (grid[0, 0] or grid[1, 0] or grid[2, 0]))):
                     return True
-
+            
             return False
-
+                
     def EraseImage(img_new, img_mark, type):
-        # unconditional_patterns = Image.getUnconditionalPatterns(type)
         for r in range(1, img_mark.shape[0]-1):
             for c in range(1, img_mark.shape[1]-1):
-                grid = img_mark[r-1:r+2, c-1:c+2]
-                # print(Image.isPatternHit(grid, type))
-                if Image.isPatternHit(grid, type):
-                    img_new[r, c] = 0
+                if img_mark[r, c]:
+                    grid = img_mark[r-1:r+2, c-1:c+2]
+                    if not Image.isPatternHit(grid, type):
+                        img_new[r, c] = 0
         
         return img_new
 
@@ -213,22 +223,57 @@ class Image:
         for _ in range(times):
             img_mark = Image.MarkImage(img_new, "Skeletonize")
             img_new = Image.EraseImage(img_new, img_mark, "Skeletonize")
+    
+        return img_new
+
+    def dilation(img, region):
+        img_new = np.zeros_like(region, dtype=int)
+        shifts = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+        for r in range(1, img.shape[0]-1):
+            for c in range(1, img.shape[1]-1):
+                if region[r, c] != 0:
+                    for dr, dc in shifts:
+                        if img[r + dr, c + dc] == 255:
+                            # print(r+dr, c+dc)
+                            img_new[r + dr, c + dc] = 255
         
         return img_new
+
+
+    def Counting(img):
+        img_label = np.zeros_like(img, dtype=int)
+        label = 1
+
+        for r in range(1, img.shape[0]-1):
+            for c in range(1, img.shape[1]-1):
+                if img[r, c] == 255 and img_label[r, c] == 0:
+                    # Find connected components
+                    region = np.zeros_like(img, dtype=int)
+                    region[r, c] = label
+
+                    while True:
+                        # print(r, c)
+                        # print("===")
+                        region_new = Image.dilation(img, region)
+                        if np.array_equal(region_new, region):
+                            break
+                        region = region_new
+                    # print("================================")
+                    # for rr in range(1, region.shape[0]-1):
+                    #     for cc in range(1, region.shape[1]-1):
+                    #         if region[rr, cc] == 255:
+                    #             print(rr, cc)
+                    img_label[region == 255] = label
+                    label += 1
+                # print("label", label)
+        
+        return label - 1
 
 def problem1_a():
     sample1 = cv2.imread("hw3_sample_images/sample1.png", cv2.IMREAD_GRAYSCALE)
     result1 = sample1.copy()
-    result1 = Image.denoise_Guassian(result1, 3)
-    threshold = 150
-    result1[result1 > threshold] = 255
-    result1[result1 <= threshold] = 0
-    
-    result1 = Image.denoise_Guassian(result1, 3)
-    threshold = 150
-    result1[result1 > threshold] = 255
-    result1[result1 <= threshold] = 0
-
+    result1 = Image.denoise_Impulse(result1, 5)
     cv2.imwrite('result1.png', result1)
 
 def problem1_b():
@@ -240,8 +285,15 @@ def problem1_c():
     result1 = cv2.imread("result1.png", cv2.IMREAD_GRAYSCALE)
     # result3 = result1.copy()
 
-    result3 = Image.Skeletonizing(result1, 20)
+    result3 = Image.Skeletonizing(result1, 50)
     cv2.imwrite('result3.png', result3)
+
+def problem1_d():
+    result2 = cv2.imread("result2.png", cv2.IMREAD_GRAYSCALE)
+    result4 = result2.copy()
+
+    result4 = Image.Counting(result4)
+    print("Number of Object: ", result4)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -252,6 +304,7 @@ if __name__ == "__main__":
         1: problem1_a,
         2: problem1_b,
         3: problem1_c,
+        4: problem1_d,
     }
 
     options[args.choice]()
